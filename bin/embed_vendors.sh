3c0d389db108fa93fbd6678162d657b3535e2216
kbond <kevinbond@gmail.com>
2010-12-13 12:25:52 -0500

    removed shell scripts in favor of ant build.xml

--- bin/embed_vendors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PROJECT_DIRECTORY=$(cd `dirname $0` && pwd)/../
-VENDOR=$PROJECT_DIRECTORY/lib/vendor
-
-mkdir $VENDOR
-
-cd $VENDOR && svn co http://svn.symfony-project.com/tags/RELEASE_1_4_8/ symfony
--- bin/install_deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PROJECT_DIRECTORY=$(cd `dirname $0` && pwd)/../
-VENDOR=$PROJECT_DIRECTORY/lib/vendor
-PLUGINS=$PROJECT_DIRECTORY/plugins
-
-#cd $PLUGINS && svn co [URL] [NAME]
-
-#cd $PLUGINS && git clone [URL] [NAME]
