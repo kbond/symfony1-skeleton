f7389ae1ffb317750b2f3354e78e457fba0f449d
Kevin Bond <kevinbond@gmail.com>
2010-12-20 06:55:40 -0800

    fixed type, changed width

--- web/version.php
+++ web/version.php
@@ -7,7 +7,7 @@ $version = file_exists("../VERSION");
 ?>
 
 <style type="text/css">
-  #ci-build { color: #666; font-size: 0.9em; text-align: left; margin: 10px 0 0 0; padding: 5px 8px; background: #eee; width: 14em; position: fixed; bottom: 0; right: 0; }
+  #ci-build { color: #666; font-size: 0.9em; text-align: left; margin: 10px 0 0 0; padding: 5px 8px; background: #eee; width: 10em; position: fixed; bottom: 0; right: 0; }
   #ci-build p { margin: 0 0 3px; font-weight: bold; }
   #ci-build dl { margin: 0; }
   #ci-build dt { font-weight: normal; float: left; width: 60px; clear: both; }
@@ -22,7 +22,7 @@ $version = file_exists("../VERSION");
       <dd><?php echo file_get_contents("../VERSION") ?></dd>
     <?php endif; ?>
     <?php if ($build): ?>
-        <dt>Version</dt>
+        <dt>Build</dt>
         <dd><?php echo file_get_contents("../BUILD_NUMBER") ?></dd>
     <?php endif; ?>
   </dl>
